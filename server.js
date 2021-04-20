92b0d9e8498e3f1bbe316c467a6002abce42b302
Maciej Zbierowski <maix19bep@gmail.com>
2021-04-20 10:48:08 +0200

    branch1: dictionary

--- server.js
+++ server.js
@@ -11,6 +11,14 @@ const io = require('socket.io')(http, {
 const PORT = process.env.PORT || 3000;
 const colors = ['red', 'green', 'blue', 'orange'];
 const players = [];
+const dictionary = {
+    CLIENT: {
+        JOIN_GAME: 'JoinGame'
+    },
+    SERVER: {
+        SEND_PLAYERS: 'SendPlayers'
+    }
+};
 
 const generateRandomNumber = (min, max) => Math.floor(Math.random() * (max - min)) + min;
 
@@ -24,15 +32,18 @@ function selectColor() {
 
 io.on("connection", socket => {
     const { clientsCount } = io.engine;
+    const { CLIENT, SERVER } = dictionary;
+    const { JOIN_GAME } = CLIENT;
+    const { SEND_PLAYERS } = SERVER;
 
     if (clientsCount > 4) {
         console.log('4 players are here');
         return;
     }
 
-    io.emit('send players', players);
+    io.emit(SEND_PLAYERS, players);
 
-    socket.on('create player', () => {
+    socket.on(JOIN_GAME, () => {
         const color = selectColor();
         const player = {
             id: socket.id,
@@ -40,7 +51,7 @@ io.on("connection", socket => {
             color
         };
         players.push(player);
-        io.emit('send players', players);
+        io.emit(SEND_PLAYERS, players);
         console.log('Players ', players);
         console.log('colors ', colors);
     });
@@ -49,7 +60,7 @@ io.on("connection", socket => {
         const playerIndex = players.findIndex(p => p.id === socket.id);
 
         players.splice(playerIndex, 1);
-        io.emit('send players', players);
+        io.emit(SEND_PLAYERS, players);
     });
 });
 
