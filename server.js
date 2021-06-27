9af07a6763d7553525a6d2e07e0254bbf497e390
Maciej Zbierowski <maix19bep@gmail.com>
2021-06-27 16:56:51 +0200

    branch4: board functionality

--- server.js
+++ server.js
@@ -1,70 +1,85 @@
-'use strict';
+"use strict";
 
-const app = require('express')();
-const http = require('http').Server(app);
-const io = require('socket.io')(http, {
-    cors: {
-        origin: '*'
-    }
+const app = require("express")();
+const http = require("http").Server(app);
+const io = require("socket.io")(http, {
+  cors: {
+    origin: "*",
+  },
 });
 
 const PORT = process.env.PORT || 3000;
-const colors = ['red', 'green', 'blue', 'orange'];
+const colors = ["red", "green", "blue", "orange"];
 const players = [];
 const dictionary = {
-    CLIENT: {
-        JOIN_GAME: 'JoinGame',
-    },
-    SERVER: {
-        SEND_PLAYERS: 'SendPlayers',
-        SEND_PLAYER_COLOR: 'SendPlayerColor',
-    }
+  CLIENT: {
+    JOIN_GAME: "JoinGame",
+    SEND_BOARD_CLIENT: "SendBoardFromClient",
+  },
+  SERVER: {
+    SEND_PLAYERS: "SendPlayers",
+    SEND_PLAYER_COLOR: "SendPlayerColor",
+    SEND_BOARD_SERVER: "SendBoardFromServer",
+  },
 };
+let serverBoard = [
+  [null, null, null, null, null, null],
+  [null, null, null, null, null, null],
+  [null, null, null, null, null, null],
+  [null, null, null, null, null, null],
+  [null, null, null, null, null, null],
+  [null, null, null, null, null, null],
+];
 
-const generateRandomNumber = (min, max) => Math.floor(Math.random() * (max - min)) + min;
+const generateRandomNumber = (min, max) =>
+  Math.floor(Math.random() * (max - min)) + min;
 
 function selectColor() {
-    const color = colors[generateRandomNumber(0, colors.length)];
-    const colorIndex = colors.indexOf(color);
+  const color = colors[generateRandomNumber(0, colors.length)];
+  const colorIndex = colors.indexOf(color);
 
-    colors.splice(colorIndex, 1);
-    return color;
+  colors.splice(colorIndex, 1);
+  return color;
 }
 
-io.on("connection", socket => {
-    const { clientsCount } = io.engine;
-    const { CLIENT, SERVER } = dictionary;
-    const { JOIN_GAME } = CLIENT;
-    const { SEND_PLAYERS, SEND_PLAYER_COLOR } = SERVER;
+io.on("connection", (socket) => {
+  const { clientsCount } = io.engine;
+  const { CLIENT, SERVER } = dictionary;
+  const { JOIN_GAME, SEND_BOARD_CLIENT } = CLIENT;
+  const { SEND_PLAYERS, SEND_PLAYER_COLOR, SEND_BOARD_SERVER } = SERVER;
 
-    if (clientsCount > 4) {
-        console.log('4 players are here');
-        return;
-    }
+  if (clientsCount > 4) {
+    console.log("4 players are here");
+    return;
+  }
 
+  io.emit(SEND_PLAYERS, players);
+
+  socket.on(JOIN_GAME, () => {
+    const color = selectColor();
+    const player = {
+      id: socket.id,
+      color,
+    };
+
+    players.push(player);
     io.emit(SEND_PLAYERS, players);
+    socket.emit(SEND_PLAYER_COLOR, player.color);
+  });
 
-    socket.on(JOIN_GAME, () => {
-        const color = selectColor();
-        const player = {
-            id: socket.id,
-            color
-        };
-        players.push(player);
-        io.emit(SEND_PLAYERS, players);
-        socket.emit(SEND_PLAYER_COLOR, player.color);
-        console.log('Players ', players);
-        console.log('colors ', colors);
-    });
+  socket.on(SEND_BOARD_CLIENT, (board) => {
+    serverBoard = board;
+    io.emit(SEND_BOARD_SERVER, serverBoard);
+  });
 
-    socket.on('disconnect', () => {
-        const playerIndex = players.findIndex(p => p.id === socket.id);
+  socket.on("disconnect", () => {
+    const playerIndex = players.findIndex((p) => p.id === socket.id);
 
-        players.splice(playerIndex, 1);
-        io.emit(SEND_PLAYERS, players);
-    });
+    players.splice(playerIndex, 1);
+    io.emit(SEND_PLAYERS, players);
+  });
 });
 
 http.listen(PORT, () => {
-    console.log(`Server is on localhost: ${PORT}`);
+  console.log(`Server is on localhost: ${PORT}`);
 });
